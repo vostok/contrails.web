47884b0685cff71a74edffcc9864445ecf5e2cbf
tihonove <tihonove@skbkontur.ru>
2017-08-27 19:24:31 +0500

    optimize production build

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,16 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 const createRules = require("./build/rules.js");
 const { extensions, createAliases } = require("./build/resolve.js");
 
-//const NODE_ENV = process.env.NODE_ENV;
+const NODE_ENV = process.env.NODE_ENV;
 
-module.exports = {
+const config = {
     entry: {
-        index: [
-            "react-hot-loader/patch",
-            "webpack-dev-server/client?http://localhost:3000",
-            "babel-polyfill",
-            "./src/index.js",
-        ],
+        index: ["babel-polyfill", "./src/index.js"],
     },
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -32,29 +28,52 @@ module.exports = {
         alias: createAliases(),
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.ModuleConcatenationPlugin(),
         new HtmlWebpackPlugin({
             template: "./src/index.html",
         }),
-        new CopyWebpackPlugin([
-            {
-                from: "./build/testing.web.config",
-                to: "web.config",
-            },
-        ]),
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+        }),
     ],
-    devServer: {
+};
+
+if (NODE_ENV === "development") {
+    config.entry.index = []
+        .concat(["react-hot-loader/patch", "webpack-dev-server/client?http://localhost:3000"])
+        .concat(config.entry.index);
+
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    );
+    config.devServer = {
         proxy: {
             "/api": {
                 target: "http://logsearchapi.dev.kontur:30002",
                 pathRewrite: { "^/api": "" },
             },
         },
-        allowedHosts: "localhost",
+        allowedHosts: ["localhost"],
         port: 3000,
         historyApiFallback: true,
         hot: true,
-    },
-};
+    };
+}
+
+if (NODE_ENV === "production") {
+    config.output.filename = "[name].[hash].js";
+    config.devtool = "cheap-module-source-map";
+    config.plugins.push(
+        new MinifyPlugin(true, { test: /\.jsx?$/i }),
+        new CopyWebpackPlugin([
+            {
+                from: "./build/testing.web.config",
+                to: "web.config",
+            },
+        ])
+    );
+}
+
+module.exports = config;
