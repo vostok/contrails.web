3e3a0d029227b4b707c1941004755f5b91c5fe03
tihonove <tihonove@skbkontur.ru>
2017-11-09 15:44:50 +0500

    Fix of strage bug

--- src/components/ContrailPanels/ContrailPanels.jsx
+++ src/components/ContrailPanels/ContrailPanels.jsx
@@ -3,50 +3,34 @@ import * as React from "react";
 
 import cn from "./ContrailPanels.less";
 
-export function ContrailPanelsContainer({ children }: { children: React.Node }): React.Node {
-    return (
-        <div className={cn("contrail-panels-container")}>
-            {children}
-        </div>
-    );
+export class ContrailPanelsContainer extends React.Component<{ children: React.Node }> {
+    props: { children: React.Node };
+
+    render(): React.Node {
+        return <div className={cn("contrail-panels-container")}>{this.props.children}</div>;
+    }
 }
 
-export function ContrailPanelsTop({ children }: { children: React.Node }): React.Node {
-    return (
-        <div className={cn("contrail-panels-top")}>
-            {children}
-        </div>
-    );
+export class ContrailPanelsTop extends React.Component<{ children: React.Node }> {
+    props: { children: React.Node };
+
+    render(): React.Node {
+        return <div className={cn("contrail-panels-top")}>{this.props.children}</div>;
+    }
 }
 
 export function ContrailPanelsBottom({ children }: { children: React.Node }): React.Node {
-    return (
-        <div className={cn("contrail-panels-bottom")}>
-            {children}
-        </div>
-    );
+    return <div className={cn("contrail-panels-bottom")}>{children}</div>;
 }
 
 export function ContrailPanelsFooter({ children }: { children: React.Node }): React.Node {
-    return (
-        <div className={cn("contrail-panels-footer")}>
-            {children}
-        </div>
-    );
+    return <div className={cn("contrail-panels-footer")}>{children}</div>;
 }
 
 export function ContrailPanelsBottomLeft({ children }: { children: React.Node }): React.Node {
-    return (
-        <div className={cn("contrail-panels-bottom-left")}>
-            {children}
-        </div>
-    );
+    return <div className={cn("contrail-panels-bottom-left")}>{children}</div>;
 }
 
 export function ContrailPanelsBottomRight({ children }: { children: React.Node }): React.Node {
-    return (
-        <div className={cn("contrail-panels-bottom-right")}>
-            {children}
-        </div>
-    );
+    return <div className={cn("contrail-panels-bottom-right")}>{children}</div>;
 }
