042819b8ee92e0de7ed51e3d0d1f7b8ac7449449
tihonove <tihonove@skbkontur.ru>
2017-11-09 16:59:01 +0500

    One more strage fix

--- src/components/ContrailPanels/ContrailPanels.jsx
+++ src/components/ContrailPanels/ContrailPanels.jsx
@@ -19,16 +19,24 @@ export class ContrailPanelsTop extends React.Component<{ children: React.Node }>
     }
 }
 
-export function ContrailPanelsBottom({ children }: { children: React.Node }): React.Node {
-    return <div className={cn("contrail-panels-bottom")}>{children}</div>;
+export class ContrailPanelsBottom extends React.Component<{ children: React.Node }> {
+    props: { children: React.Node };
+
+    render(): React.Node {
+        return <div className={cn("contrail-panels-bottom")}>{this.props.children}</div>;
+    }
 }
 
 export function ContrailPanelsFooter({ children }: { children: React.Node }): React.Node {
     return <div className={cn("contrail-panels-footer")}>{children}</div>;
 }
 
-export function ContrailPanelsBottomLeft({ children }: { children: React.Node }): React.Node {
-    return <div className={cn("contrail-panels-bottom-left")}>{children}</div>;
+export class ContrailPanelsBottomLeft extends React.Component<{ children: React.Node }> {
+    props: { children: React.Node };
+
+    render(): React.Node {
+        return <div className={cn("contrail-panels-bottom-left")}>{this.props.children}</div>;
+    }
 }
 
 export function ContrailPanelsBottomRight({ children }: { children: React.Node }): React.Node {
