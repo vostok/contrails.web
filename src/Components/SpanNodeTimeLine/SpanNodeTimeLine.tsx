7e0226191fe316b18ca78add0893121926178833
tihonove <tihonove@skbkontur.ru>
2019-09-29 13:16:12 +0500

    Время на полосках

--- src/Components/SpanNodeTimeLine/SpanNodeTimeLine.tsx
+++ src/Components/SpanNodeTimeLine/SpanNodeTimeLine.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { useComponentWidth } from "../../Commons/Hooks/UseComponentWidth";
 import { Colors } from "../../Domain/Colors";
+import { DateTimeUtils } from "../../Domain/DateTimeUtils";
 import { TimeRange } from "../../Domain/TimeRange";
 import { SpanNode } from "../../Domain/TraceTree/SpanNode";
 
@@ -18,8 +19,17 @@ export function SpanNodeTimeLine({ node, totalTimeRange, highlighted }: SpanNode
 
     const timeRangeDuration = totalTimeRange.to - totalTimeRange.from;
     const nodeDuration = node.to - node.from;
-    const left = Math.round(((node.from - totalTimeRange.from) / timeRangeDuration) * width);
-    const timeLineWidth = Math.max(4, (nodeDuration / timeRangeDuration) * width);
+
+    const nodeLeftInPixels = Math.round(((node.from - totalTimeRange.from) / timeRangeDuration) * width);
+    const nodeWidthInPixels = Math.max(4, (nodeDuration / timeRangeDuration) * width);
+
+    const leftOversize = Math.abs(Math.min(0, nodeLeftInPixels));
+    const nodeLeftPosition = nodeLeftInPixels + leftOversize;
+    const nodeWidthPosition = nodeWidthInPixels - leftOversize;
+
+    const durationFormatted = DateTimeUtils.formatDurationTicks(
+        DateTimeUtils.difference(node.source.EndTimestamp, node.source.BeginTimestamp)
+    );
 
     return (
         <div ref={container} className={cn("container")}>
@@ -27,9 +37,17 @@ export function SpanNodeTimeLine({ node, totalTimeRange, highlighted }: SpanNode
                 className={cn("time-span", { faded: !highlighted })}
                 style={{
                     backgroundColor: Colors[node.colorConfig].background,
-                    width: timeLineWidth,
-                    left: left,
+                    width: nodeWidthPosition,
+                    left: nodeLeftPosition,
                 }}>
+                <span
+                    className={cn("duration", {
+                        left: nodeLeftPosition >= 40,
+                        right: nodeLeftPosition < 40 && width - (nodeLeftPosition + nodeWidthPosition) >= 40,
+                        inside: nodeLeftPosition < 40 && width - (nodeLeftPosition + nodeWidthPosition) < 40,
+                    })}>
+                    <span className={cn("inner")}>{durationFormatted}</span>
+                </span>
             </div>
         </div>
     );
