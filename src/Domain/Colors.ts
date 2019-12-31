10b4efc5b6f321b0b5e6a39828a7dd30d284b48d
kungurtsev <kungurtsev@skbkontur.ru>
2019-12-31 17:50:43 +0500

    fix colors

--- src/Domain/Colors.ts
+++ src/Domain/Colors.ts
@@ -1,24 +1,24 @@
 export const Colors = [
     {
         text: "white",
-        background: "rgba(0, 170, 144, 0.50)",
+        background: "rgba(0, 120, 0, 0.50)",
         hoverText: "white",
-        hoverBackground: "rgba(0, 170, 144, 0.80)",
-        border: "rgba(0,170,144,1.0)",
+        hoverBackground: "rgba(0, 120, 0, 0.80)",
+        border: "rgba(0, 120, 0, 1.0)",
     },
     {
         text: "white",
-        background: "rgba(215, 12, 23, 0.50)",
+        background: "rgba(200, 0, 0, 0.50)",
         hoverText: "white",
-        hoverBackground: "rgba(215, 12, 23, 0.80)",
-        border: "rgba(215, 12, 23, 1.0)",
+        hoverBackground: "rgba(200, 0, 0, 0.80)",
+        border: "rgba(200, 0, 0, 1.0)",
     },
     {
         text: "white",
-        background: "rgba(255, 85, 0, 0.50)",
+        background: "rgba(255, 130, 0, 0.50)",
         hoverText: "white",
-        hoverBackground: "rgba(255, 85, 0, 0.80)",
-        border: "rgba(255,85,0,1.0)",
+        hoverBackground: "rgba(255, 130, 0, 0.80)",
+        border: "rgba(255, 130, 0, 1.0)",
     },
     {
         text: "white",
